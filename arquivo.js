4ca553289f329dc8ebbf1f215dc777f6987e7d08
IolandaRichelly <iolandarichelly12@gmail.com>
2016-09-05 17:36:14 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+function calcular(){
+	var iptx= document.getElementById("iptx");
+	var ipty= document.getElementById("ipty");
+	
+	var x= iptx.value;
+	var y= ipty.value;
+	
+	var r=x=y;
+	document.getElementByid("res").innerHTMl="O resultado da operação é"+r ;
+	
+}
